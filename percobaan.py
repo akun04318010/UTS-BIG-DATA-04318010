4b0c864281f0dabb50354195cbef2ff94aea3061
akun04318010 <82586407+akun04318010@users.noreply.github.com>
2021-04-26 15:33:05 +0700

    Add files via upload

---
+++ percobaan.py
@@ -0,0 +1,16 @@
+# import StemmerFactory class
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+# create stemmer
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+# stemming process
+sentence = ' Saya Bangga jadi warga nahdlatul ulama'
+output   = stemmer.stem(sentence)
+
+print(output)
+#  Saya Bangga jadi warga nahdlatul ulama
+
+print(stemmer.stem('Mereka meniru-nirukannya'))
+# mereka tiru
